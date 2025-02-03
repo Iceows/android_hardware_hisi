c7a31aafc3eec22a2302eae8f39b139894b0fcb6
Iceows <mounierr07@gmail.com>
2025-02-03 22:48:39 +0100

    hisi: Try to add libstagefrighthw

---
+++ libstagefrighthw/Android.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2008 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(BOARD_USES_OMXHW),)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    Hisi_OMX_Plugin.cpp
+
+LOCAL_CFLAGS += $(PV_CFLAGS_MINUS_VISIBILITY)
+
+LOCAL_C_INCLUDES:= \
+      frameworks/native/include/media/hardware \
+      frameworks/native/include/media/openmax \
+      frameworks/native/include
+
+LOCAL_SHARED_LIBRARIES :=    \
+        libbinder            \
+        libutils             \
+        libcutils            \
+        libui                \
+        libdl                \
+        libstagefright_foundation \
+        liblog
+
+LOCAL_MODULE := libstagefrighthw
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
+endif # BOARD_USES_OMXHW
